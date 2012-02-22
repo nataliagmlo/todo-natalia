a70a83ddbeaf3adf1084c441f3c3af99683ae650
nataliagmlo <nataliagmlo@gmail.com>
2012-02-22 19:53:34 +0100

    Añadimos rails al proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodoNatalia::Application.config.secret_token = '9b534f8f77bcace783377a27d59833e90b48a924eb315ed244789c94dce18ea380ecb0bae4ecfe5a5bf80e8dff0a23d520fafc6270b4f6efc325fd9a76f1cca7'
