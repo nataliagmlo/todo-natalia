c516d235e317f6471aedab8d3385da5cf1559071
nataliagmlo <nataliagmlo@gmail.com>
2012-02-24 20:24:21 +0100

    Añadir scope

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,17 @@ class Task < ActiveRecord::Base
 	belongs_to :user 
 	belongs_to :proyect
 
+	scope :nearly_completed, :conditions => ["percentage >= ?", 90]
+	scope :not_assigned, :conditions => ["user_id IS NULL"]
+	scope :assigned, :conditions => ["user_id IS NOT NULL"]
+	# scope :almost_due, :conditions => ["due_at < ?", 7.days.from_now] Esta mal porque la fecha se guarda la primera vez
+	#Solucion
+	scope :almost_due, lambda {
+			{:conditions => ["due_at < ?", 7.days.from_now]}
+		}
+
+	# Saca las tareas de un usuario
+	scope :for_user, lambda { |user|
+		{:conditions => ["user_id = ?", user.id]}
+	}
 end
